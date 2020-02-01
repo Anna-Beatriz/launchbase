dfee52a4bcde82bba434d4f31071b498d329d0a1
Anna-Beatriz <annabeatriz.ms1@gmail.com>
2020-01-31 23:49:37 -0300

    Operadores. Liberar ou não entrada por idade.

---
+++ operadores.js
@@ -0,0 +1,10 @@
+const idade = 16
+
+// Verificar se a idade é maior ou igual a 18 anos
+if (idade >= 18) {
+    // Se sim, deixar entrar
+    console.log('Entrada liberada.')
+} else {
+    // Se não, bloquear a entrada.
+    console.log('Entrada não autorizada.')
+}
