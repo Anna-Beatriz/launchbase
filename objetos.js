0b5e01df75aeefd8e14fdf5b4986e9c197fbe5de
Anna-Beatriz <annabeatriz.ms1@gmail.com>
2020-02-01 13:54:46 -0300

    Utilizando objetos para média dos alunos.

---
+++ objetos.js
@@ -0,0 +1,20 @@
+// Objetos
+
+const aluno1 = {
+    nome: "Anna",
+    nota: "9.8"
+}
+
+const aluno2 = {
+    nome: "Beatriz",
+    nota: "10"
+}
+
+const aluno3 = {
+    nome: "Luna",
+    nota: "2"
+}
+
+const media = (aluno1.nota + aluno2.nota + aluno3.nota) / 3
+
+console.log(aluno2)
