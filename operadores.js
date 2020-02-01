cd2c9dad87a32894e44836e7f5c53ede1282780a
Anna-Beatriz <annabeatriz.ms1@gmail.com>
2020-02-01 00:00:07 -0300

    Liberando entrada por idade e descobrindo se a idade é igual a 17.

--- operadores.js
+++ operadores.js
@@ -1,4 +1,4 @@
-const idade = 16
+const idade = 17
 
 // Verificar se a idade é maior ou igual a 18 anos
 if (idade >= 18) {
@@ -6,5 +6,11 @@ if (idade >= 18) {
     console.log('Entrada liberada.')
 } else {
     // Se não, bloquear a entrada.
-    console.log('Entrada não autorizada.')
+    console.log(`Idade: ${idade} anos. Entrada não autorizada.`)
+}
+
+// Se a pessoa tiver 17 anos
+if (idade == 17) {
+    // avisar para voltar quando fizer 18 anos
+    console.log('Volte quando completar 18 anos!')
 }
