d7e4efe963f7575aa4c53c7dde5a3461a6157f0e
Anna-Beatriz <annabeatriz.ms1@gmail.com>
2020-01-31 23:13:48 -0300

    Cálculo da média dos alunos.

---
+++ index.js
@@ -0,0 +1,18 @@
+// Programa que calcula a média das notas
+// entre alunos e
+// envia mensagem do cálculo.
+
+const aluno1 = 'Anna'
+const notaAluno1 = 9.8
+
+const aluno2 = 'Beatriz'
+const notaAluno2 = 10
+
+const aluno3 = 'Luna'
+const notaAluno3 = 2
+
+// const nomes = `Anna ${nome2}` -> Template string(chamar outra variável)
+
+const media = (notaAluno1 + notaAluno2 + notaAluno3) / 3
+
+console.log(media)
