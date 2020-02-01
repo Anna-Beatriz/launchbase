5a716a25536ee862fd9d00b1580b3b63e768c07a
Anna-Beatriz <annabeatriz.ms1@gmail.com>
2020-02-01 13:38:12 -0300

    Verificando idade com apenas um if.

---
+++ operadores2.js
@@ -0,0 +1,17 @@
+const idade = 15
+
+// Verificar se a idade é maior ou igual a 18 anos
+// Se sim, deixar entrar
+// Se não, bloquear a entrada.
+// Se a pessoa tiver 17 anos
+// avisar para voltar quando fizer 18 anos
+
+if (!(idade >= 18) || idade === 17) {
+
+    console.log('Entrada não autorizada.')
+
+} else {
+
+    console.log('Entrada liberada.')
+
+}
